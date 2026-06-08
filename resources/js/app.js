779fcfcff675cdf78b524f9688afe26884cc8230
Darivs <darivsxp@gmail.com>
2026-06-08 21:01:00 +0800

    Initial Laravel Vue portfolio setup

---
+++ resources/js/app.js
@@ -0,0 +1,9 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
+
+    < !--resources / js / App.vue-- >
+    <template>
+        <h1>My Portfolio</h1>
+    </template>
